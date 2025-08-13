6ef4e17d76551405236e1abaa63e84e01f2a8f4a
willerdantas <44482854+willerdantas@users.noreply.github.com>
2025-08-13 00:40:15 +0000

    Cadastro de variáveis e primeira carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,34 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    printf("Programa Cartas Super Trunfo \n");
+    printf("Cadastro de cartas \n");
+    printf("\n");
+    
+    char estado1, estado2, cod_carta1[3], cod_carta2[3], nome_cidade1[30], nome_cidade2[30];
+    int populacao1, populacao2, ponto_turistico1, ponto_turistico2;
+    float area1, area2, pib1, pib2;
+
+    printf("Didite uma letra de A a H que identifica o estado: \n");
+    scanf("%s", &estado1);
+
+    printf("Digite o código da carta: \n");
+    scanf("%s", &cod_carta1);
+
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", &nome_cidade1);
+
+    printf("Digite a população da cidade: \n");
+    scanf("%d", &populacao1);
+
+    printf("Digite a área da cidade: \n");
+    scanf("%f", &area1);
+
+    printf("Digite o PIB da cidade: \n");
+    scanf("%f", &pib1);
+
+    printf("Digite o número de pontos turísticos: \n");
+    scanf("%d", &ponto_turistico1);
+
     return 0;
 }
